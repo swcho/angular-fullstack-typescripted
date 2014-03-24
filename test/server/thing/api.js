000e32200b575e338eba0b53415ebc4515e40e9a
sungwoo <sungwoo.cho.dev@gmail.com>
2014-03-24 23:06:22 +0900

    TypeScript applied for test code

--- test/server/thing/api.js
+++ test/server/thing/api.js
@@ -1,20 +1,18 @@
-'use strict';
+/// <reference path="../../../typings/node/node.d.ts" />
+/// <reference path="../../../typings/mocha/mocha.d.ts" />
+/// <reference path="../../../typings/should/should.d.ts" />
+/// <reference path="../../../typings/supertest/supertest.d.ts" />
+var app = require('../../../server');
+var request = require('supertest');
 
-var should = require('should'),
-    app = require('../../../server'),
-    request = require('supertest');
-
-describe('GET /api/awesomeThings', function() {
-  
-  it('should respond with JSON array', function(done) {
-    request(app)
-      .get('/api/awesomeThings')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
-        done();
-      });
-  });
+describe('GET /api/awesomeThings', function () {
+    it('should respond with JSON array', function (done) {
+        request(app).get('/api/awesomeThings').expect(200).expect('Content-Type', /json/).end(function (err, res) {
+            if (err)
+                return done(err);
+            res.body.should.be.instanceof(Array);
+            done();
+        });
+    });
 });
+//# sourceMappingURL=api.js.map
--- test/server/user/model.ts
+++ test/server/user/model.ts
@@ -2,16 +2,17 @@
 /// <reference path="../../../typings/mocha/mocha.d.ts" />
 /// <reference path="../../../typings/should/should.d.ts" />
 /// <reference path="../../../typings/mongoose/mongoose.d.ts" />
-var should = require('should');
-var mongoose = require('mongoose');
-var user = require('../../../lib/models/user');
 
-var User = mongoose.model('User');
+import should = require('should');
+import mongoose = require('mongoose');
+import user = require('../../../lib/models/user');
+
+var User = mongoose.model<user.TUser>('User');
 
 var sUser;
 
-describe('User Model', function () {
-    before(function (done) {
+describe('User Model', function() {
+    before(function(done) {
         sUser = new User({
             provider: 'local',
             name: 'Fake User',
@@ -24,41 +25,42 @@ describe('User Model', function () {
         done();
     });
 
-    afterEach(function (done) {
+    afterEach(function(done) {
         User.remove({}).exec();
         done();
     });
 
-    it('should begin with no users', function (done) {
-        User.find({}, function (err, users) {
+    it('should begin with no users', function(done) {
+        User.find({}, function(err, users) {
             users.should.have.length(0);
             done();
         });
     });
 
-    it('should fail when saving a duplicate user', function (done) {
+    it('should fail when saving a duplicate user', function(done) {
         sUser.save();
         var userDup = new User(user);
-        userDup.save(function (err) {
+        userDup.save(function(err) {
             should.exist(err);
             done();
         });
     });
 
-    it('should fail when saving without an email', function (done) {
+    it('should fail when saving without an email', function(done) {
         sUser.email = '';
-        sUser.save(function (err) {
+        sUser.save(function(err) {
             should.exist(err);
             done();
         });
     });
 
-    it("should authenticate user if password is valid", function () {
+    it("should authenticate user if password is valid", function() {
         sUser.authenticate('password').should.be.true;
     });
 
-    it("should not authenticate user if password is invalid", function () {
+    it("should not authenticate user if password is invalid", function() {
         sUser.authenticate('blah').should.not.be.true;
     });
+
 });
-//# sourceMappingURL=model.js.map
+
